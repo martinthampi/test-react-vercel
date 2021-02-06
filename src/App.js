790002590c92fbb7517b6382efe5162fb94621ac
Martin KP <69423299+martinthampi@users.noreply.github.com>
2021-02-06 14:44:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vercel</p>
+        <a
+          className="App-link"
+          href="https://martinkp.hashnode.dev/host-react-apps-for-free-on-vercel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Host React Apps for Free on Vercel!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
